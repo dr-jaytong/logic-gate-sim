d908b600853c8b7b9b5913e6daeccad4a080df22
dr-jaytong <jason.g.tong@gmail.com>
2025-07-11 20:36:48 -0700

    Added string utility functions for stripping different characters

--- utilities/Utilities.cpp
+++ utilities/Utilities.cpp
@@ -1,1 +1,73 @@
+#include "Utilities.hpp"
+#include <iostream>
+#include <sstream>
+#include <algorithm>
 
+namespace Utilities {
+
+std::string GetFirstWordFromLine(std::string const &sLine)
+{
+    size_t const iStartPosition(sLine.find_first_not_of(" \t\n\r\f\v"));             // Find first non-white space character
+    size_t const iEndPosition  (sLine.find_first_of(" \t\n\r\f\v", iStartPosition)); // Find first position of white space after first character
+
+    if (iEndPosition == std::string::npos)
+        return sLine.substr(iStartPosition, sLine.length());
+
+    return RemoveWhiteSpace(sLine.substr(iStartPosition, iEndPosition - iStartPosition));
+}
+
+std::string RemoveWhiteSpace(std::string const &sLine)
+{
+    return StripString(sLine, ' ');
+}
+
+std::string StripString(std::string const &sLine, std::vector<char> const &vPatterns)
+{
+    std::string sCleanLine(sLine);
+    for (auto const &sPattern : vPatterns) {
+        std::cout << "################# stripping : " << sPattern << std::endl;
+        sCleanLine = StripString(sCleanLine, sPattern);
+
+    }
+
+
+    return sCleanLine;
+}
+
+std::string StripString(std::string const &sLine, char const &cDelimeter)
+{
+    std::string sCleanLine(sLine);
+    std::cout << "Before: \'" <<  sCleanLine << "\'" << std::endl;
+    sCleanLine.erase(std::remove(sCleanLine.begin(), sCleanLine.end(), cDelimeter), sCleanLine.end());
+    std::cout << "After: \'" << sCleanLine << "\'" << std::endl;
+    return sCleanLine; 
+}
+
+std::string StripString(std::string const &sLine, std::string const &sWordToRemove)
+{
+    std::string sCleanLine(sLine);
+    size_t const uiPos(sCleanLine.find(sWordToRemove));
+
+    if (uiPos != std::string::npos) {
+        std::cout << "Before: \'" <<  sCleanLine << "\'" << std::endl;
+        sCleanLine.erase(uiPos, sWordToRemove.length());
+        std::cout << "After: \'" << sCleanLine << "\'" << std::endl;
+    }
+
+    return sCleanLine;
+}
+
+std::vector<std::string> TokenizeString(std::string const &sLine, char const &cDelimeter)
+{
+    std::vector<std::string> vTokens;
+    std::stringstream ssLine(sLine);
+    std::string sToken("");
+    
+    while (std::getline(ssLine, sToken, cDelimeter)) {
+        vTokens.emplace_back(sToken);
+    }
+
+    return vTokens;
+}
+
+};
--- utilities/Utilities.hpp
+++ utilities/Utilities.hpp
@@ -1,8 +1,13 @@
+#include <string>
+#include <vector>
+
 
 namespace Utilities
 {
-
-
-
-
+    std::string GetFirstWordFromLine(std::string const &sLine);
+    std::string StripString(std::string const &sLine, std::vector<char> const &vPatterns);
+    std::string StripString(std::string const &sLine, char const &cDelimeter);
+    std::string StripString(std::string const &sLine, std::string const &sWord);
+    std::string RemoveWhiteSpace(std::string const &sLine);
+    std::vector<std::string> TokenizeString(std::string const &sLine, char const &cDelimeter);
 };
