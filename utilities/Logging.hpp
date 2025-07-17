c7124e6f9d25545ba1701ebcda47be284841a1c1
dr-jaytong <jason.g.tong@gmail.com>
2025-07-16 22:33:22 -0700

    Adding basic logging function

---
+++ utilities/Logging.hpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <sstream>
+
+#ifndef CPP_MODULE
+#define CPP_MODULE "UNKN"
+#endif
+
+// Basic logging message
+#define LOG(msg) std::cout << CPP_MODULE << ": " << msg << "\n";
+
+// Logging with error, throws an exception as well
+#define LOG_ERROR(msg) {\
+    std::stringstream sErrorMessage;\
+    sErrorMessage << CPP_MODULE << " ERROR(" << __LINE__<< "): " <<  msg << "\n"; \
+    std::cerr << sErrorMessage.str(); \
+    throw std::runtime_error(sErrorMessage.str()); }\
+
+// Assertion check
+#define LOG_ASSERT(cond, msg)\
+    do { \
+        if (!(cond)) { \
+            std::cerr << CPP_MODULE << " ERROR(" << __LINE__<< "): " <<  msg << "\n"; \
+            exit(EXIT_FAILURE); \
+        } \
+    } while(0) \
+
